b26c816f81c7e48e4eee8176e3f652749ab2d307
Marcelo <marcelodossantosmachado05@gmail.com>
2025-10-04 18:42:57 -0300

    feat:Criando projeto

---
+++ Style_Lab/src/test/java/com/ecommerce/Style_Lab/StyleLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommerce.Style_Lab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
